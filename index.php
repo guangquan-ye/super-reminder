27395b58800f336248c0a7b002917e66a940ac49
Guangquan Ye <shinz@MacBook-Pro-de-Guangquan.local>
2023-09-26 11:59:16 +0200

    add some auth features

--- index.php
+++ index.php
@@ -74,21 +74,87 @@ if(isset($_POST["login"])){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="./style/style.css">
-    <script defer src="./script/header.js"></script>
     <script defer src="./script/auth.js"></script>
-    <script defer src="./script/todolist.js"></script>
-
     <title>Todolist</title>
 </head>
 
 <body>
     <h1>Welcome To my Todolist</h1>
 
-    <a href="" id="signUpDisplay">Inscription</a>
-    <a href="" id="signInDisplay">Connexion</a>
-  </div>
- 
-  <div id="formDisplayDiv"></div>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" id="signInFormDisplay">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login</h2>
+      <h2 class="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-sky-500" id="signInMsg"></h2>
+    </div>
+  
+    <div class="mt-1 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST" id="signInForm" action="index.php">
+        <div>
+          <label for="login" class="block text-sm font-medium leading-6 text-gray-900">Login</label>
+          <div class="mt-2">
+            <input name="signInLogin" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+  
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          </div>
+          <div class="mt-2">
+            <input  name="signInPwd" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+  
+        <div>
+          <button type="submit" id="signInBtn" class="flex w-full justify-center rounded-md bg-yellow-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
+        </div>
+      </form>
+      <p class="mt-10 text-center text-sm text-gray-500">
+      Not a member?
+      <a href="" id="signUpDisplayBtn" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign up</a>
+    </p>
+    </div>
+</div>
+
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" id="signUpFormDisplay">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register</h2>
+          <h2 class="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900" id="signUpMsg"></h2>
+        </div>
+      
+        <div class="mt-1 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form class="space-y-6" action="#" method="POST" id="signUpForm" action="index.php">
+            <div>
+              <label for="login" class="block text-sm font-medium leading-6 text-gray-900">Login</label>
+              <div class="mt-2">
+                <input name="signUpLogin" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+              </div>
+            </div>
+      
+            <div>
+              <div class="flex items-center justify-between">
+                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              </div>
+              <div class="mt-2">
+                <input  name="signUpPwd" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+              </div>
+              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password Confirm</label>
+              <div class="mt-2">
+                <input  name="signUpPwdConf" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+              </div>
+            </div>
+      
+            <div>
+              <button type="submit" id="signUpBtn" class="flex w-full justify-center rounded-md bg-yellow-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600">Sign up</button>
+            </div>
+          </form>
+          <p class="mt-10 text-center text-sm text-gray-500">
+            Already a member?
+      <a href="" id="signInDisplayBtn" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign In</a>
+    </p>
+        </div>
+</div>
+
 
 </body>
 
--- script/auth.js
+++ script/auth.js
@@ -1,87 +1,72 @@
-const signUpDisplay = document.querySelector('#signUpDisplay');
-const signInDisplay = document.querySelector('#signInDisplay');
-const formDisplayDiv = document.querySelector('#formDisplayDiv');
+const signUpFormDisplay = document.querySelector('#signUpFormDisplay');
+const signInFormDisplay = document.querySelector('#signInFormDisplay');
+const signInBtn = document.querySelector('#signInBtn');
+const signUpBtn = document.querySelector('#signUpBtn');
+const signUpDisplayBtn = document.querySelector('#signUpDisplayBtn');
+const signInDisplayBtn = document.querySelector('#signInDisplayBtn');
 
-
-function toggleDisplay() {
-    if (formDisplayDiv.style.display === "block") {
-        formDisplayDiv.style.display = "none";
-    }
-    else {
-        formDisplayDiv.style.display = "block";
-    }
+function hideSignUpForm() {
+    signUpFormDisplay.style.display = "none";
 }
 
-
-signUpDisplay.addEventListener('click', (ev) => {
+signInDisplayBtn.addEventListener('click', (ev) => {
     ev.preventDefault();
-    toggleDisplay();
-
-    fetch('./inscription.html', {
-        method: 'GET',
-
-    }).then((response) => {
-        return response.text();
-
-    }
-    ).then((formDisplay) => {
-       
-        formDisplayDiv.innerHTML = "";
-        formDisplayDiv.innerHTML = formDisplay;
-        const signUpBtn = document.querySelector('#signUpBtn');
-        signUpBtn.addEventListener('click', (ev) => {
-            ev.preventDefault();
-            const signUpForm = document.querySelector('#signUpForm');
-            const formData = new FormData(signUpForm);
-            formData.append('register', "ok");
-            fetch('./index.php', {
-                method: 'POST',
-                body: formData
-            }).then((response) => {
-                return response.text();
-            }).then((data) => {
-                const signUpMsg = document.querySelector('#signUpMsg');
-                signUpMsg.innerHTML = "";
-                signUpMsg.innerHTML = data;
-                
-            });
-        });
-    })
+    signInFormDisplay.style.display = "block";
+    signUpFormDisplay.style.display = "none";
 });
 
-signInDisplay.addEventListener('click', (ev) => {
+signUpDisplayBtn.addEventListener('click', (ev) => {
     ev.preventDefault();
-    toggleDisplay();
-
-    fetch('./connexion.html', {
-        method: 'GET',
-
-    }).then((response) => {
-        return response.text();
-
-    }
-    ).then((formDisplay) => {
-        const formDisplayDiv = document.querySelector('#formDisplayDiv');
-        formDisplayDiv.innerHTML = "";
-        formDisplayDiv.innerHTML = formDisplay;
-        const signInBtn = document.querySelector('#signInBtn');
-        signInBtn.addEventListener('click', (ev) => {
-            ev.preventDefault();
-            const signInForm = document.querySelector('#signInForm');
-            const formData = new FormData(signInForm);
-            formData.append('login', "ok");
-            fetch('./index.php', {
-                method: 'POST',
-                body: formData
-            }).then((response) => {
-                return response.text();
-            }).then((data) => {
-                const signInMsg = document.querySelector('#signInMsg');
-                signInMsg.innerHTML = "";
-                signInMsg.innerHTML = data;
-                
-            });
-        });
-    })
+    signUpFormDisplay.style.display = "block";
+    signInFormDisplay.style.display = "none";
 });
 
+signUpBtn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    const signUpForm = document.querySelector('#signUpForm');
+    const formData = new FormData(signUpForm);
+    formData.append('register', "ok");
+    fetch('./index.php', {
+        method: 'POST',
+        body: formData
+    }).then((response) => {
+        return response.text();
+    }).then((data) => {
+        const signUpMsg = document.querySelector('#signUpMsg');
+        signUpMsg.innerHTML = "";
+        signUpMsg.innerHTML = data;
+
+        setTimeout(function() {
+            window.location.href = './index.php';
+        }, 2000);
+
+    });
+});
+
+
+
+
+signInBtn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    const signInForm = document.querySelector('#signInForm');
+    const formData = new FormData(signInForm);
+    formData.append('login', "ok");
+    fetch('./index.php', {
+        method: 'POST',
+        body: formData
+    }).then((response) => {
+        return response.text();
+    }).then((data) => {
+        const signInMsg = document.querySelector('#signInMsg');
+        signInMsg.innerHTML = "";
+        signInMsg.innerHTML = data;
+
+        setTimeout(function() {
+            window.location.href = './todo.php';
+        }, 2000);
+
+    });
+});
+
+
+hideSignUpForm();
